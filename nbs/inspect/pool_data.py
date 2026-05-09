7b446f7fc3f0c2d4206a4d411b597650724c0036
Danielle Rothermel <danielle.rothermel@gmail.com>
2026-05-09 16:43:38 -0400

    add pool status to pool data notebook

--- nbs/inspect/pool_data.py
+++ nbs/inspect/pool_data.py
@@ -1,24 +1,31 @@
 import marimo
 
-__generated_with = "0.23.2"
+__generated_with = "0.23.5"
 app = marimo.App(width="columns")
 
 with app.setup:
     import marimo as mo
 
-
-@app.cell
-def _():
-    return
+    from dr_llm.pool.admin_service import inspect_pool
+    from dr_llm.pool.models import PoolInspectionRequest
 
 
 @app.cell
 def _():
-    return
+    PROJECT_NAME = "code_comp_v0"
+    POOL_NAME = "encoder_pool_t1"
+    return POOL_NAME, PROJECT_NAME
 
 
-@app.cell(column=1)
-def _():
+@app.cell(column=1, hide_code=True)
+def _(POOL_NAME, PROJECT_NAME):
+    pool_status = inspect_pool(
+        PoolInspectionRequest(
+            project_name=PROJECT_NAME,
+            pool_name=POOL_NAME,
+        )
+    )
+    pool_status
     return
 
 
